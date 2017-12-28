cd0fd3b8f33854a2b36b35566fc68fe77943af17
Cheetar <mateusz@sieniawski.net>
2017-12-28 05:00:42 +0100

    Implemented basic tracker models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,36 @@
+from django.contrib import settings
 from django.db import models
 
-# Create your models here.
+from djmoney.models.fields import MoneyField
+
+
+class Progile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+
+class Board(models.Model):
+    owner = models.ForeignKey(Progile)
+    name = models.CharField(max_length=150)
+    date_create = models.DateTimeField()
+    date_updated = models.DateTimeField()
+    # add currency field
+
+
+class Category(models.Model):
+    board = models.ForeignKey(Board)
+    name = models.CharField(max_length=150)
+
+
+class SubCategory(models.Model):
+    board = models.ForeignKey(Board)
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=150)
+
+
+class Spending(models.Model):
+    board = models.ForeignKey(Board)
+    category = models.ForeignKey(Category)
+    sub_category = models.ForeignKey(SubCategory)
+    name = models.CharField(max_length=150)
+    cost = MoneyField(decimal_places=2, default_currency='PLN', max_digits=11)
+    is_income = models.BooleanField(default=False)
