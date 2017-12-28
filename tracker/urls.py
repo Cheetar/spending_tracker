8d3394b91d03e1e51c88d33ff65780f2b5361a68
Cheetar <mateusz@sieniawski.net>
2017-12-28 13:27:32 +0100

    Added home view to urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-from . import registration_views
+from . import registration_views, views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/', registration_views.profile, name='profile'),
     path('accounts/register/', registration_views.register, name='register'),
