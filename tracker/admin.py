eca349a3396db82a84b90d22c7c1e75718acbe6f
Cheetar <mateusz@sieniawski.net>
2017-12-30 00:17:14 +0100

    Developed models and registered them at admin site

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Board, Category, Profile, Spending, SubCategory
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board')
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board', 'category')
+
+
+class SpendingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'board', 'category', 'sub_category', 'cost', 'date', 'is_income')
+
+
+admin.site.register(Profile)
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Spending, SpendingAdmin)
--- tracker/models.py
+++ tracker/models.py
@@ -1,26 +1,35 @@
-from datetime import datetime
-
 from django.conf import settings
 from django.db import models
 
 from djmoney.models.fields import MoneyField
 
 
-class Progile(models.Model):
+class Profile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Board(models.Model):
-    owner = models.ForeignKey(Progile, on_delete=models.PROTECT)
+    owner = models.ForeignKey(Profile, on_delete=models.PROTECT)
     name = models.CharField(max_length=150)
-    date_create = models.DateTimeField()
-    date_updated = models.DateTimeField()
     # add currency field
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
     board = models.ForeignKey(Board, on_delete=models.PROTECT)
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class SubCategory(models.Model):
@@ -28,12 +37,25 @@ class SubCategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Sub Category'
+        verbose_name_plural = 'Sub Categories'
+
 
 class Spending(models.Model):
     board = models.ForeignKey(Board, on_delete=models.PROTECT)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT, blank=True, null=True)
     name = models.CharField(max_length=150)
     cost = MoneyField(decimal_places=2, default_currency='PLN', max_digits=11)
-    date = models.DateField(default=datetime.now())
+    date = models.DateField(auto_now_add=True)
     is_income = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['date']
