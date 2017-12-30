e6bcacb09480d38f5bff2cdba2a56c26df6f8625
Cheetar <mateusz@sieniawski.net>
2017-12-30 15:01:52 +0100

    Authenticated user is redirected from home to dashboard

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
     return render(request, 'tracker/home.html')
 
 
